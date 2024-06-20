cd8a54bea186f312067ee2b38f4782344bc374fe
Diki Dwi Diro <dikidwid0@gmail.com>
2024-06-20 11:19:51 +0700

    Initial Commit

---
+++ HoneyDue/HoneyDueApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyDueApp.swift
+//  HoneyDue
+//
+//  Created by Diki Dwi Diro on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneyDueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
